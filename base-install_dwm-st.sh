5be400ec19e5027fa5e5186d55fe0ac7e9486a70
Patrick White <15720524+pwhite1989@users.noreply.github.com>
2022-01-14 00:14:15 +0000

    Update base-install_dwm-st.sh

--- base-install_dwm-st.sh
+++ base-install_dwm-st.sh
@@ -1,4 +1,5 @@
-  # Login as root
+#!/bin/bash
+# Login as root
 xbps-install -Suy
   # run again
 xbps-install -Suy
@@ -10,3 +11,22 @@ xbps-install -S elogind polkit
 
   # Link the services to enable them
 ln -s /etc/sv/{dbus,elogind,polkitd} /var/service/
+
+  # Install the WM and tools
+xbps-install -S xorg xinit dwm st git dmenu xf86-video-intel firefox feh xdg-user-dirs wget curl vim
+
+wget git.io/voidlinux -O void.png
+feh --bg-scale void.png
+mkdir -p .scripts
+
+cat <<! > .scripts/xsetloop.sh
+#!/bin/bash
+!
+chmod +x .scripts/xsetloop.sh
+
+
+cat <<! > .xinitrc
+xrandr --output Virtual1 --mode 2560x1440
+${HOME}/.fehbg
+setxkbmap -layout gb
+exec dwm
