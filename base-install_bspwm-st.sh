850f01d05b0bc35be9c4cb37848e8cf7abe41952
Patrick White <15720524+pwhite1989@users.noreply.github.com>
2022-01-16 14:08:21 +0000

    Update base-install_bspwm-st.sh

--- base-install_bspwm-st.sh
+++ base-install_bspwm-st.sh
@@ -21,7 +21,7 @@ ln -s /etc/sv/{dbus,elogind,polkitd} /var/service/
 xbps-install -Sy make pkg-config cparser
 
   # Install the WM and tools
-xbps-install -Sy xorg xinit bspwm sxhkd lightdm lightdm-gtk3-greeter lightdm-gtk-greeter-settings lxappearance picom polybar git rofi xf86-video-intel firefox feh xdg-user-dirs wget curl vim unzip bat neofetch
+xbps-install -Sy xorg xinit bspwm sxhkd lightdm lightdm-gtk3-greeter lightdm-gtk-greeter-settings lxappearance picom polybar git rofi xf86-video-intel firefox feh xdg-user-dirs wget curl vim unzip bat neofetch svn
   # Organise folders and put in wm config
 cd ${HOME}
 xdg-user-dirs-update
@@ -56,30 +56,49 @@ exec bspwm
   # Installing the st terminal
 xbps-install -Sy libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel
 git clone https://github.com/siduck/st.git
-cd st
+cd ${HOME}/st
 sudo make install 
 xrdb merge xresources
 
-  # Installing the font
+  # Installing the fonts
 cd ${HOME}/Downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
 mkdir JetBrainsMono
 unzip JetBrainsMono.zip -d JetBrainsMono
 mv JetBrainsMono /usr/share/fonts
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
+mkdir Iosevka
+unzip Iosevka.zip -d Iosevka
+mv Iosevka /usr/share/fonts
 fc-cache -fv
+
+cd ${HOME}
 
 ehco 'xrdb merge ${HOME}/st/xresources' >> .bashrc
 source .bashrc
 
-  # Make sure all folders are owned by the user
-chown -R ${USER}:${USER} ${HOME}
-
   # TODO Polybar Config
+svn https://github.com/siduck/dotfiles/trunk/polybar/ .config
+xbps-install -S xprop wmctrl slop
+    
   # TODO Rofi Config
+svn https://github.com/siduck/dotfiles/trunk/rofi/ .config
+sed -i 's/Sarasa Nerd Font 14/Iosevka 12/g' .config/rofi/config.rasi
+sed -i 's/forest/onedark/g' .config/rofi/config.rasi
+
   # TODO Picom Config
+svn https://github.com/siduck/dotfiles/trunk/picom/ .config
+
   # TODO CLI Tools/Ranger Config
+
   # TODO .bashrc config
   # TODO nvchad install and config
+
+rm .config/.svn
+
+  # Make sure all folders are owned by the user
+chown -R ${USER}:${USER} ${HOME}
 
   # Link the lightdm service
 ln -s /etc/sv/lightdm /var/service/
