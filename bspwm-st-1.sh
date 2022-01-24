c0a38d039c4fbad93deb1627c5e4fcb059190467
Patrick White <pwhite1989@gmail.com>
2022-01-24 20:37:16 +0000

    reorganised the files

--- bspwm-st-1.sh
+++ bspwm-st-1.sh
@@ -36,58 +36,18 @@ xbps-install -Suy &&
   # Core installation
   # Seat manager: elogind (includes dbus by default)
   # Policy Manager: polkit
+  # Time daemon: chrony
 xbps-install -Sy elogind polkit chrony &&
 
   # Link the services to enable them
 ln -s /etc/sv/{dbus,elogind,polkitd,chronyd} /var/service/
 
   # Install C compilers, the WM and tools
-xbps-install -Sy make pkg-config cparser xorg xinit bspwm sxhkd lightdm lightdm-gtk3-greeter lightdm-gtk-greeter-settings lxappearance picom polybar git rofi xf86-video-intel firefox feh xdg-user-dirs wget curl vim unzip bat neofetch ranger subversion htop fzf tabbed xprop wmctrl slop neovim &&
+xbps-install -Sy make pkg-config cparser xorg xinit bspwm sxhkd lightdm lightdm-gtk3-greeter lightdm-gtk-greeter-settings lxappearance picom polybar git rofi xf86-video-intel firefox feh xdg-user-dirs wget curl vim unzip bat neofetch ranger subversion htop fzf tabbed xprop wmctrl slop neovim xclip zsh &&
 
-  # Organise folders and put in wm config
 cd ${USERHOME}
-mkdir -p .config/{bspwm,sxhkd,gtk,alsa_stuff,eww,ranger,polybar,rofi,picom}
-mkdir -p {Downloads,Pictures}
 
-  # Bspwm config
-svn checkout https://github.com/siduck/dotfiles/trunk/bspwm/ .config/bspwm/
-sed -i '22d' .config/bspwm/bspwmrc
-sed -i '22i feh --bg-scale ~/void.png &' .config/bspwm/bspwmrc
-sed -i '3d' .config/bspwm/bspwmrc
-
-  # Sxhkd config
-mkdir -p .config/sxhkd
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd/
-sed -i 's|dmenu_run|rofi -show drun|g' .config/sxhkd/sxhkdrc
-sed -i 's/urxvt/st/g' .config/sxhkd/sxhkdrc
-
-
-wget git.io/voidlinux -O void.png
-
-cat <<EOF > .xinitrc
-#!/bin/sh
-#
-# ~/.xinitrc
-#
-# Executed by startx (run your window manager from here)
-
-if [ -d /etc/X11/xinit/xinitrc.d ];
-then
-for f in /etc/X11/xinit/xinitrc.d/*; do
-    [ -x "$f" ] && . "$f"
-  done
-  unset f
-fi
-
-xrandr --output Virtual1 --mode bar
-${USERHOME}/.fehbg
-setxkbmap -layout gb
-exec bspwm
-EOF
-
-sed -i 's|bar|'"${RESOLUTION}"'|g' .xinitrc
-
-  # Installing the st terminal
+ # Installing the st terminal
 xbps-install -Sy libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel &&
 git clone https://github.com/siduck/st.git &&
 cd ${USERHOME}/st
@@ -109,8 +69,7 @@ fc-cache -fv
 
 cd ${USERHOME}
 
-  # .bashrc config
-    #Install logo-ls
+  #Install logo-ls
 cd ${USERHOME}/Downloads
 wget https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_Linux_x86_64.tar.gz &&
 tar -xzf logo-ls_Linux_x86_64.tar.gz
@@ -118,50 +77,9 @@ cd logo-ls_Linux_x86_64
 cp logo-ls /usr/local/bin
 cp logo-ls.1.gz /usr/share/man/man1/
 cd ${USERHOME}
-    # add the config
-cat <<EOF! > .bashrc
-    # draw horiz line under prompt
-draw_line() {
-  local COLUMNS="$COLUMNS"
-  while ((COLUMNS-- > 0)); do
-    printf '\e[30m\u2500'
-  done
-}
-
-    # prompt
-PS1="\[\033[32m\]  \[\033[37m\]\[\033[34m\]\w \[\033[0m\]"
-PS2="\[\033[32m\]  > \[\033[0m\]"
-
-    # bash history
-HISTSIZE=
-HISTFILESIZE=
-
-alias ls='logo-ls'
-alias la='logo-ls -A'
-alias ll='logo-ls -al'
-    # equivalents with Git Status on by Default
-alias lsg='logo-ls -D'
-alias lag='logo-ls -AD'
-alias llg='logo-ls -alD'
-
-export FZF_DEFAULT_OPTS='
-  --color fg:#646a76
-  --color bg+:#7797b7,fg+:#2c2f30,hl:#D8DEE9,hl+:#26292a,gutter:#3a404c
-  --color pointer:#373d49,info:#606672
-  --border
-  --color border:#646a76'
-  
-xrdb merge ${USERHOME}/st/xresources
-EOF!
-
-  # Get part 2
-wget https://raw.githubusercontent.com/pwhite1989/void-base-install/main/bspwm-st-2.sh
-chmod +x bspwm-st-2.sh
 
   # Make sure all folders are owned by the user
 chown -R ${USERNAME}:${USERNAME} ${USERHOME}
 
   # Link the lightdm service
 ln -s /etc/sv/lightdm /var/service/
-
-
--- bspwm-st-2.sh
+++ bspwm-st-2.sh
@@ -2,44 +2,26 @@
 
 xdg-user-dirs-update
 
-wget https://raw.githubusercontent.com/siduck/dotfiles/master/.Xresources
+git clone https://github.com/pwhite1989/void-base-install.git
+#wget https://raw.githubusercontent.com/siduck/dotfiles/master/.Xresources
+
+# http://stackoverflow.com/a/844299
+expand-or-complete-with-dots() {
+  echo -n "\e[31m...\e[0m"
+    zle expand-or-complete
+      zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
 
 DISPLAYNAME=$(xrandr -q | awk '/connected primary/{print $1}')
-DISPLAYNAME=${DISPLAYNAME:-Virtual1}
+DISPLAYNAME=${DISPLAYNAME:-"Virtual1"}
 
 sed -i 's|Virtual1|'"${DISPLAYNAME}"'|g' .xinitrc
 
-  # get some configuration folders
-svn checkout https://github.com/siduck/dotfiles/trunk/gtk/ .config/gtk
-svn checkout https://github.com/siduck/dotfiles/trunk/alsa_stuff/ .config/alsa_stuff
-svn checkout https://github.com/siduck/dotfiles/trunk/eww/ .config/eww
-
-  # Ranger installation
-svn checkout https://github.com/siduck/dotfiles/trunk/cli_tools/ranger .config/ranger
-ranger --copy-config=rifle
-ranger --copy-config=commands
-ranger --copy-config=scope
-
-  # Polybar Config
-svn checkout https://github.com/siduck/dotfiles/trunk/polybar/polybar .config/polybar
-sed -i 's/killall -q/pkill/g' .config/polybar/launch.sh
-sed -i 's|eDP1|'"${DISPLAYNAME}"'|g' .config/polybar/config
-    
-  # Rofi Config
-svn checkout https://github.com/siduck/dotfiles/trunk/rofi/ .config/rofi
-sed -i 's/Sarasa Nerd Font 14/Iosevka 12/g' .config/rofi/config.rasi
-sed -i 's/forest/onedark/g' .config/rofi/config.rasi
-
-  # Picom Config
-svn checkout https://github.com/siduck/dotfiles/trunk/picom/ .config/picom
-
-  
-  # TODO nvchad install and config
+  # Nvim installation 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync &&
   # TODO install eww https://elkowar.github.io/eww/
 
-
-rm -rf .config/{bspwm,gtk,alsa_stuff,eww,ranger,polybar,rofi,picom}/.svn
-source .bashrc
 source .xinitrc
